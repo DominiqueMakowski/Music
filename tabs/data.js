ba168eaa59bed087c926cd706243681b2d40c0b0
DominiqueMakowski <dom.mak19@gmail.com>
2025-11-22 10:58:35 +0000

    tabs

---
+++ tabs/data.js
@@ -0,0 +1,84 @@
+const DATA = [
+    {
+        artist: "3 Doors Down",
+        title: "Kryptonite",
+        lyrics: `
+Well, I took a walk around the world to ease my troubled mind
+I left my body lying somewhere in the sands of time
+But I watched the world float to the dark side of the moon
+I feel there's nothing I can do, yeah
+
+I watched the world float to the dark side of the moon
+After all I knew, it had to be something to do with you
+I really don't mind what happens now and then
+As long as you'll be my friend at the end
+
+<b>If I go crazy, then will you still call me Superman?
+If I'm alive and well, will you be there and holding my hand?
+I'll keep you by my side with my superhuman might
+Kryptonite</b>
+
+You called me strong, you called me weak
+But still your secrets, I will keep
+You took for granted all the times, I never let you down
+You stumbled in and bumped your head
+If not for me then you'd be dead
+I picked you up and put you back on solid ground
+
+<b>CHORUS x2</b>
+`,
+        tabs: `
+VERSE: Bm - G - A 
+CHORUS: Bm - G - A / Bm - Em- F#5
+`,
+    },
+    {
+        artist: "4 Non Blondes",
+        title: "What's Up",
+        lyrics: `
+25 years and my life is still
+Tryin' to get up that great big hill of hope
+For a destination
+
+I realized quickly when I knew I should
+That the world was made up of this brotherhood of man
+For whatever that means
+
+And so I cry sometimes when I'm lying in bed
+Just to get it all out what's in my head
+And I, I am feeling a little peculiar
+
+And so I wake in the morning and I step outside
+And I take a deep breath and I get real high
+And I scream from the top of my lungs
+"What's going on?"
+
+<b>And I say, hey-ey-ey, Hey-ey-ey
+I said "Hey, 
+what's going on?"
+x2</b>
+
+And I try
+Oh my God, do I try
+I try all the time
+In this institution
+
+And I pray
+Oh my God, do I pray
+I pray every single day
+For revolution
+
+Bridge
+<b>CHORUS x2</b>
+
+25 years and my life is still
+Tryin' to get up that great big hill of hope
+For a destination, mmm
+`,
+        tabs: `
+E - F#m - A - E
+`,
+    },
+]
+
+window.DATA = DATA
