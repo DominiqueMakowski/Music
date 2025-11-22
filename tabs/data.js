a4740b1a71159b828d6226bbb035b26232bad5a6
DominiqueMakowski <dom.mak19@gmail.com>
2025-11-22 13:29:36 +0000

    minor

--- tabs/data.js
+++ tabs/data.js
@@ -81,6 +81,96 @@ Tryin' to get up that great big hill of hope
 For a destination, mmm
 `,
     },
+    {
+        artist: "ABBA",
+        title: "Take a Chance on Me",
+        tags: ["disco"],
+        tabs: `
+VERSE: Dm - C (x2)
+BRIDGE: Am - F - Am - F -- Am - F - (Dm - G) x2
+CHORUS: C - G - C (x2) // Dm - G (x2)
+`,
+        lyrics: `
+We can go dancing, we can go walking, as long as we're together
+Listen to some music, maybe just talking, get to know you better
+
+<i>'Cos you know I've got - 
+So much that I wanna do, when I dream I'm alone with you
+It's magic
+You want me to leave it there, afraid of a love affair
+But I think you know
+That I can't let go</i>
+
+<b>If you change your mind, I'm the first in line
+Honey I'm still free
+Take a chance on me
+If you need me, let me know, gonna be around
+If you've got no place to go, if you're feeling down
+If you're all alone when the pretty birds have flown
+Honey I'm still free
+Take a chance on me
+Gonna do my very best and it ain't no lie
+If you put me to the test, if you let me try
+Take a chance on me
+Take a chance on me</b>
+
+Oh you can take your time baby, I'm in no hurry, know I'm gonna get you
+You don't wanna hurt me, baby don't worry, I ain't gonna let you
+
+<i>Let me tell you now - 
+My love is strong enough to last when things are rough
+It's magic
+You say that I waste my time but I can't get you off my mind
+No I can't let 
+go 'Cos I love you so</i>
+
+<b>CHORUS</b>
+`,
+    },
+    {
+        artist: "Ace of Base",
+        title: "All That She Wants",
+        tags: [],
+        tabs: `
+VERSE: C# - B - F# / C# - G# / C# - F# - F#m / C# - G# - F# - G#
+CHORUS: C#m -  B - F#m
+
+`,
+        lyrics: `
+<b>She leads a lonely life
+She leads a lonely life</b>
+
+When she woke up late in the morning light
+And the day had just begun
+She opened up her eyes and thought
+Oh, what a morning 
+
+It's not a day for work
+It's a day for catching sun
+Just laying on the beach and having fun
+She's going to get you  
+
+<b>All that she wants is another baby
+She's gone tomorrow, boy
+All that she wants is another baby
+All that she wants is another baby
+She's gone tomorrow, boy
+All that she wants is another baby</b>
+
+So if you are in sight and the day is right
+She's a hunter, you're the fox
+The gentle voice that talks to you
+Won't talk forever
+
+It is a night for passion
+But the morning means goodbye
+Beware of what is flashing in her eyes
+She's going to get you
+
+<b>CHORUS (soft)
+CHORUS (loud)</b>
+`,
+    },
 ]
 
 window.DATA = DATA
